89dd748e123f7b91885dc252c311ba4bbc534baf
tvist <tvistmail@gmail.com>
2016-05-19 18:14:52 +0300

    Chat

---
+++ src/com/chat/Client.java
@@ -0,0 +1,26 @@
+package com.chat;
+
+import java.net.Socket;
+
+/**
+ * Created by tvist on 19.05.16.
+ */
+public class Client {
+
+    private String stringConnect;
+    private int portConnect;
+
+
+    public Client(){
+         Client("127.0.0.1",8888);
+    }
+
+    public Client(String stringConnect, int portConnect){
+        this.stringConnect = stringConnect;
+        this.portConnect = portConnect;
+    }
+
+    public void connect(){
+        Socket socketClient = new Socket();
+    }
+}
